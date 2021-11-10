c0499ce2c8080c11981fd2005b2da7f1cbf93c6f
Danilo Almeida <danilo.a.leite@gmail.com>
2021-11-10 12:06:45 -0300

    refactor: final project

--- 03-four-main-verbs/src/main/java/br/com/danilo/foo/controller/PersonController.java
+++ 03-four-main-verbs/src/main/java/br/com/danilo/foo/controller/PersonController.java
@@ -1,26 +1,50 @@
 package br.com.danilo.foo.controller;
 
-import br.com.danilo.foo.exception.UnsupportedMathGenerationException;
-import br.com.danilo.foo.math.SimpleMath;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import br.com.danilo.foo.model.Person;
+import br.com.danilo.foo.services.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 
-import static br.com.danilo.foo.converter.NumberConverter.convertToDouble;
-import static br.com.danilo.foo.converter.NumberConverter.isNumeric;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
+@RequestMapping("/person")
 public class PersonController {
+    @Autowired
+    private PersonService service;
 
-    private SimpleMath math = new SimpleMath();
-
-    @RequestMapping(value = "/sum/{numberOne}/{numberTwo}", method = RequestMethod.GET)
-    public Double sum(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) throws Exception {
-        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
-            throw new UnsupportedMathGenerationException("Please set numeric value");
-        }
-        return math.sum(convertToDouble(numberOne), convertToDouble(numberTwo));
+    @RequestMapping(method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Person> findaLL() {
+        return service.findAll();
     }
 
+    @RequestMapping(value = "/{id}",
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public Person findById(@PathVariable("id") String id) {
+        return service.findById(id);
+    }
+
+    @RequestMapping(method = RequestMethod.POST,
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public Person create(@RequestBody Person person) {
+        return service.create(person);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT,
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public Person update(@RequestBody Person person) {
+        return service.update(person);
+    }
+
+    @RequestMapping(value = "/{id}",
+            method = RequestMethod.DELETE)
+    public void delete(@PathVariable("id") String id) {
+        service.delete(id);
+    }
 }
--- 03-four-main-verbs/src/main/java/br/com/danilo/foo/services/PersonService.java
+++ 03-four-main-verbs/src/main/java/br/com/danilo/foo/services/PersonService.java
@@ -4,15 +4,56 @@ package br.com.danilo.foo.services;
 import br.com.danilo.foo.model.Person;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 @Service
 public class PersonService {
     private final AtomicLong counter = new AtomicLong();
 
-    public Person findById( String id) {
+
+
+    public Person create (Person person) {
+
+        return person;
+    }
+
+    public Person update (Person person) {
+
+        return person;
+    }
+
+    public void delete (String id) {
+
+    }
+
+    public List<Person> findAll() {
+        List<Person> persons = new ArrayList<Person>();
+        for (int i = 0; i < 8; i++) {
+            Person person = mockPerson(i);
+            persons.add(person);
+        }
+        return persons;
+    }
+
+    public Person findById(String id) {
         Person person = new Person();
         person.setId(counter.incrementAndGet());
+        person.setFirstName("Danilo");
+        person.setLastName("Almeida");
+        person.setAddress("Rua qualquer numero 123");
+        person.setGender("male");
+        return person;
+    }
+
+    private Person mockPerson(int i) {
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("Person name " + i);
+        person.setLastName("Last Name " + i);
+        person.setAddress("Some address in Brazil " + i);
+        person.setGender("gender");
         return person;
     }
 }
