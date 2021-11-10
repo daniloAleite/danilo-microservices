86a2a9b406dabe57fdbd34a9731a5ea3c97740f4
Danilo Almeida <danilo.a.leite@gmail.com>
2021-11-10 16:06:15 -0300

    refactor: initial structure

---
+++ 04-greeting-service/src/test/java/greetingservice/greetingservice/ApplicationTests.java
@@ -0,0 +1,13 @@
+package greetingservice.greetingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
