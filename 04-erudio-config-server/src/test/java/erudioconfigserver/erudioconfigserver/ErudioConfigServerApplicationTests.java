4021b2237738c2320342da917b9a114f0e855445
Danilo Almeida <danilo.a.leite@gmail.com>
2021-11-11 15:22:55 -0300

    refactor: initial structure

---
+++ 04-erudio-config-server/src/test/java/erudioconfigserver/erudioconfigserver/ErudioConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package erudioconfigserver.erudioconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErudioConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
